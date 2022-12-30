015be98ae393359b27e1ddd805352557b8db9ac0
DanDan <you@example.com>
2022-12-30 03:12:14 +0300

    done lesson 2

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import requests
-import json
 from urllib.parse import urlparse
 
+
+def bitlink_checker(input_url):
+    if input_url[:7] == 'bit.ly/':
+        return True
 
 def count_clicks(token, url):
     headers = {
         "Authorization":f"Bearer {token}",
     }
+    if not bitlink_checker(url):
+        json_data = {
+            "long_url":url,
+        }
+        response_get_bitlink = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers=headers, json=json_data)
+        response_get_bitlink.raise_for_status
+        bitlink = response_get_bitlink.json()["id"]
+        return bitlink
+    else:
+        bitlink = url
+    response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary', headers=headers)
+    response.raise_for_status
 
-    json_data = {
-        "long_url":url,
-    }
-    response_get_bitlink = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers=headers, json=json_data)
-    bitlink = response_get_bitlink.json()["id"]
-    response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary')
-    print(urlparse(f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'))
-    print(response.raise_for_status)
-    return response.text
+    return f'Total clicks : {response.json()["total_clicks"]}'
+
 
 def main():
     token = input()
