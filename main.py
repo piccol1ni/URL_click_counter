3f5f0ed5c64514f28c03d11285495e61d529e771
DanDan <you@example.com>
2023-01-05 15:18:38 +0300

    work with try except

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ from urllib.parse import urlparse
 
 def bitlink_checker(url_for_request: str):
     response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{url_for_request}/clicks/summary', headers=headers)
-    try:
-        response.raise_for_status()
-        return True
-    except Exception:
-        pass
+    response.raise_for_status()
+    return True
+
 
 
 def shorten_link(url_for_request: str):
@@ -18,11 +16,7 @@ def shorten_link(url_for_request: str):
         "long_url":url_for_request,
     }
     response = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers=headers, json=long_url)
-    try:
-        response.raise_for_status()
-    except Exception:
-        print('THIS IS FAIL LINK BRO, try something like https://google.com')
-        return 'THIS IS FAIL LINK BRO'
+    response.raise_for_status()
     bitlink = response.json()["id"]
     print(bitlink)
     return bitlink
@@ -41,7 +35,11 @@ if __name__=='__main__':
         "Authorization":f"Bearer {token}",
     }
     url_for_request = input()
-    if bitlink_checker(url_for_request):
-        count_clicks(url_for_request)
-    else:
-        shorten_link(url_for_request)
+    try:
+        if bitlink_checker(url_for_request):
+            count_clicks(url_for_request)
+    except requests.exceptions.HTTPError:
+        try:
+            shorten_link(url_for_request)
+        except requests.exceptions.HTTPError:
+            print('THIS IS FAIL LINK BRO, try something like https://google.com')           
