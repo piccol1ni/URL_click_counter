4998f239f3d8787d97a8559f236e967254a06074
DanDan <you@example.com>
2023-01-07 00:41:31 +0300

    fix last 3 fixes

--- main.py
+++ main.py
@@ -5,16 +5,14 @@ from dotenv import load_dotenv
 from urllib.parse import urlparse
 
 
-def is_bitlink(url_for_request: str):
-    token = os.environ["BITLY_TOKEN"]
+def is_bitlink(url_for_request: str, token: str):
     headers = {
         "Authorization": f"Bearer {token}",
     }
     response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{url_for_request}', headers=headers)
     return response.ok
 
-def shorten_link(url_for_request: str):
-    token = os.getenv("BITLY_TOKEN")
+def shorten_link(url_for_request: str, token: str):
     headers = {
         "Authorization": f"Bearer {token}",
     }
@@ -27,8 +25,7 @@ def shorten_link(url_for_request: str):
     return bitlink
 
 
-def count_clicks(url_for_request: str):
-    token = os.getenv("BITLY_TOKEN")
+def count_clicks(url_for_request: str, token: str):
     headers = {
         "Authorization": f"Bearer {token}",
     }
@@ -40,10 +37,11 @@ def count_clicks(url_for_request: str):
 if __name__=='__main__':
     load_dotenv()
     url_for_request = input()
-    if is_bitlink(url_for_request) is True:
-        print(f'Total clicks: {count_clicks(url_for_request)}')
-    else:
-        try:
-            print(shorten_link(url_for_request))
-        except requests.exceptions.HTTPError as ex:
-            print(ex)           
+    token = os.getenv("BITLY_TOKEN")
+    try:
+        if is_bitlink(url_for_request, token):
+            print(f'Total clicks: {count_clicks(url_for_request, token)}')
+        else:
+            print(shorten_link(url_for_request, token))
+    except requests.exceptions.HTTPError as ex:
+        print(ex)
