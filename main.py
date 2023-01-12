1e2a35d74072ce8aa91b2c13f2efd8a5bec159e9
DanDan <you@example.com>
2023-01-12 13:08:54 +0300

    add pep8 sting size

--- main.py
+++ main.py
@@ -11,11 +11,16 @@ def is_bitlink(url_for_request: str, token: str):
         "Authorization": f"Bearer {token}",
     }
     construct_url = urlparse(f'{url_for_request}')
+
     if construct_url.netloc:
-        response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{construct_url.netloc}{construct_url.path}', headers=headers)
+        response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{construct_url.netloc}\
+                                {construct_url.path}',
+                                headers=headers)
     else :
-        response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{construct_url.path}', headers=headers)
+        response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{construct_url.path}',
+         headers=headers)
     return response.ok
+
 
 def shorten_link(url_for_request: str, token: str):
     headers = {
@@ -24,32 +29,38 @@ def shorten_link(url_for_request: str, token: str):
     long_url = {
         "long_url": url_for_request,
     }
-    response = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers=headers, json=long_url)
+    response = requests.post('https://api-ssl.bitly.com/v4/bitlinks',
+                            headers=headers, json=long_url)
     response.raise_for_status()
     bitlink = response.json()["id"]
     return bitlink
+
 
 def count_clicks(url_for_request: str, token: str):
     headers = {
         "Authorization": f"Bearer {token}",
     }
     construct_url = urlparse(f'{url_for_request}')
-    response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{construct_url.netloc}{construct_url.path}/clicks/summary', headers=headers)
+    response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{construct_url.netloc}\
+                            {construct_url.path}/clicks/summary',
+                            headers=headers)
     response.raise_for_status()
     return response.json()["total_clicks"]
 
 
 if __name__=='__main__':
     load_dotenv()
-    parser = argparse.ArgumentParser(description='Make shorten URLs using bit.ly or show count of clocks if it is bit.ly URL')
+    parser = argparse.ArgumentParser(description='''Make shorten URLs using bit.ly
+                                     or show count of clocks if it is bit.ly URL''')
     parser.add_argument('link', help='Your url or bit.ly url')
+
+
     args = parser.parse_args()
-    url_for_request = args.link
     token = os.getenv("BITLY_TOKEN")
     try:
-        if is_bitlink(url_for_request, token):
-            print(f'Total clicks: {count_clicks(url_for_request, token)}')
+        if is_bitlink(args.link, token):
+            print(f'Total clicks: {count_clicks(args.link, token)}')
         else:
-            print(shorten_link(url_for_request, token))
+            print(shorten_link(args.link, token))
     except requests.exceptions.HTTPError as ex:
         print(ex)
